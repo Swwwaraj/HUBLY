55ef86b7980d3f4bf029bc180250b0be4bdda342
Swaraj Zavare <95712177+Swwwaraj@users.noreply.github.com>
2025-05-02 12:54:47 +0530

    Update api.js
    backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api"
+const API_URL = process.env.REACT_APP_BACKEND_URL || "https://hubly-backend-4cx3.onrender.com/api"
 
 // Create axios instance
 const api = axios.create({
